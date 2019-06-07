1e920f724218816948cdb12ca9ee2ad2d865d3a0
amateur-dev <dipesh.sukhani@gmail.com>
2019-06-07 16:06:28 +0800

    Sanity Setup and Gatsby init

---
+++ sanity-portfolio-backend/schemas/project.js
@@ -0,0 +1,51 @@
+export default {
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+    {
+        name: 'title',
+        title: 'Title',
+        type: 'string'
+        },
+    {
+        name: 'mainImage',
+        title: 'Project Screenshot',
+        type: 'image',
+        options: {
+          hotspot: true
+        },
+    },
+    {
+        name: 'description',
+        title: 'Description',
+        type: 'blockContent'
+        },
+    {
+        name: 'author',
+        title: 'Author',
+        type: 'reference',
+        to: {type: 'author'}
+        },
+        {
+            name: 'categories',
+            title: 'Categories',
+            type: 'array',
+            of: [{type: 'reference', to: {type: 'category'}}]
+          },
+    ],
+
+    preview: {
+        select: {
+          title: 'title',
+          author: 'author.name',
+          media: 'mainImage'
+        },
+        prepare(selection) {
+          const {author} = selection
+          return Object.assign({}, selection, {
+            subtitle: author && `by ${author}`
+          })
+        }
+      }
+    }
