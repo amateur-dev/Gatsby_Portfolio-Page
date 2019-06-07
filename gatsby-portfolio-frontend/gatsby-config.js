b320a3096ed8e676a66b7174bafd48f8c61b150e
amateur-dev <dipesh.sukhani@gmail.com>
2019-06-07 17:09:22 +0800

    Sourcing in the Gatsby Plugin

--- gatsby-portfolio-frontend/gatsby-config.js
+++ gatsby-portfolio-frontend/gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '798708xm',
+        dataset: 'production',
+        // a token with read permissions is required
+        // if you have a private dataset
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- sanity-portfolio-backend/schemas/project.js
+++ sanity-portfolio-backend/schemas/project.js
@@ -19,7 +19,7 @@ export default {
     {
         name: 'description',
         title: 'Description',
-        type: 'blockContent'
+        type: 'text'
         },
     {
         name: 'author',
